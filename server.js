dd1f9ec7355c05294ee1b30ad0c9b4452eff1413
Daria Lobanova <dahahbabah@yandex.ru>
2023-01-13 14:25:29 +0300

    Фикс порта в server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(`${__dirname}/dist`))
 
@@ -9,6 +9,6 @@ app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/dist/index.html`)
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
