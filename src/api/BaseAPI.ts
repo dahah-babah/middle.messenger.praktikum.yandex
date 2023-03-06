67f670b22a2ce7520afad1012b5191f0439b9d69
Daria Lobanova <dahahbabah@yandex.ru>
2023-03-06 21:02:22 +0300

    Добавлены тесты для модуля HTTP

--- src/api/BaseAPI.ts
+++ src/api/BaseAPI.ts
@@ -1,9 +1,12 @@
-import HTTPTransport from '@/core/HTTP'
+import { HTTPTransport } from '@/core/HTTP'
+import { API_URL } from '@/constants/url'
 
 abstract class BaseAPI {
   protected http: HTTPTransport
 
-  protected constructor(endpoint: string) {
+  protected constructor(path: string) {
+    const endpoint = `${API_URL}${path}`
+
     this.http = new HTTPTransport(endpoint)
   }
 }
